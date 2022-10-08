acd72245a0c308aa3384465d79a8abf2b4237264
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-08 13:47:56 -0700

    1st commit

--- main.py
+++ main.py
@@ -2,11 +2,6 @@
 # ******************************
 # Make your Code
 # ******************************
-strval = input().split()
-numbers = []
-for v in strval:
-	numbers.append(int(v))
-print (numbers)
-# the below llin 11 are same as the lines from 5 to 8
-# numbers = list(map(int, strval))
-# print (numbers)
+strval = input().split(" ")
+numbers = list(map(int, strval))
+print(numbers.count(int(input)))
